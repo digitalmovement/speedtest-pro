d7df3dc56298c9520a6cc2c0b3b58b732a4298c0
digitalmovement <33831659+digitalmovement@users.noreply.github.com>
2024-10-05 01:04:51 +0100

    Update class-wpspeedtestpro-settings.php

--- admin/class-wpspeedtestpro-settings.php
+++ admin/class-wpspeedtestpro-settings.php
@@ -84,7 +84,13 @@ class Wpspeedtestpro_Settings {
      */
     public function enqueue_scripts() {
         wp_enqueue_script( $this->plugin_name . '-settings', plugin_dir_url( __FILE__ ) . 'js/wpspeedtestpro-settings.js', array( 'jquery' ), $this->version, false );
+        wp_localize_script($this->plugin_name . '-latency-testing', 'wpspeedtestpro_ajax', array(
+            'ajax_url' => admin_url('admin-ajax.php'),
+            'nonce' => wp_create_nonce('wpspeedtestpro_nonce'),
+            'selected_region' => get_option('wp_hosting_benchmarking_selected_region') // Pass the selected region     
+        )); 
     }
+
 
     /**
      * Render the settings page for this plugin.
