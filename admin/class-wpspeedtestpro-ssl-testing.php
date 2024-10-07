204e64074e254bac7f31810aae8e4c0527f2c6e6
digitalmovement <33831659+digitalmovement@users.noreply.github.com>
2024-10-08 00:26:42 +0100

    Update class-wpspeedtestpro-ssl-testing.php

--- admin/class-wpspeedtestpro-ssl-testing.php
+++ admin/class-wpspeedtestpro-ssl-testing.php
@@ -110,7 +110,7 @@ class Wpspeedtestpro_SSL_Testing {
         $email = "jdoe@digitalmovement.co.uk";
 
         //$result = $this->core->api->test_ssl_certificate(home_url(), $email);
-        $result = $this->core->api->test_ssl_certificate("dreambook.com", $email);
+        $result = $this->core->api->test_ssl_certificate("https://dreambook.com", $email);
 
 
         if (is_array($result) && isset($result['status']) && $result['status'] !== 'READY') {
