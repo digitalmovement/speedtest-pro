1ce3230438f4a225cffa0de0bb5191e1d0b84c43
digitalmovement <33831659+digitalmovement@users.noreply.github.com>
2024-10-19 18:04:08 +0100

    Update class-wpspeedtestpro-uptime-monitoring.php

--- admin/class-wpspeedtestpro-uptime-monitoring.php
+++ admin/class-wpspeedtestpro-uptime-monitoring.php
@@ -50,8 +50,7 @@ class Wpspeedtestpro_Uptime_Monitoring {
     private function is_this_the_right_plugin_page() {
         if ( function_exists( 'get_current_screen' ) ) {
             $screen = get_current_screen();
-            print_r($screen);
-            return $screen && $screen->id === 'wp-speed-test-pro_page_wpspeedtestpro-page-uptime-monitoring';    
+            return $screen && $screen->id === 'wp-speed-test-pro_page_wpspeedtestpro-uptime-monitoring';    
         }
     }
 
