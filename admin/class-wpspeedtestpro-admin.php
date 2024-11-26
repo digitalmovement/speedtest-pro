29e1e681cf84ed7f1429664ed809937b98f14548
digitalmovement <33831659+digitalmovement@users.noreply.github.com>
2024-11-26 23:12:30 +0000

    Update class-wpspeedtestpro-admin.php

--- admin/class-wpspeedtestpro-admin.php
+++ admin/class-wpspeedtestpro-admin.php
@@ -460,9 +460,12 @@ class Wpspeedtestpro_Sync_Handler {
 
             if (is_wp_error($response)) {
                 throw new Exception('Failed to send data: ' . $response->get_error_message());
-                error_log('WPSpeedTestPro Sync Error: ' . $response->get_error_message());
             }
 
+            if (wp_remote_retrieve_response_code($response) !== 200) {
+                error_log('WPSpeedTestPro Sync Error: ' . wp_remote_retrieve_response_message($response));
+            }
+            
             $body = json_decode(wp_remote_retrieve_body($response), true);
 
             if (!empty($body['success'])) {
