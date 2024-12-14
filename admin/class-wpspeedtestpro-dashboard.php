7c6b7b89128b3b9ec7874fc93f88e599703691e6
digitalmovement <33831659+digitalmovement@users.noreply.github.com>
2024-12-14 23:44:11 +0000

    Update class-wpspeedtestpro-dashboard.php

--- admin/class-wpspeedtestpro-dashboard.php
+++ admin/class-wpspeedtestpro-dashboard.php
@@ -43,7 +43,7 @@ class Wpspeedtestpro_Dashboard {
 
     private $uptime_monitoring;
     private $bug_report_handler;
-    
+
     /**
      * Initialize the class and set its properties.
      *
@@ -89,7 +89,7 @@ class Wpspeedtestpro_Dashboard {
     public function enqueue_styles() {
         if ($this->is_this_the_right_plugin_page()) {
             wp_enqueue_style( $this->plugin_name . '-dashboard', plugin_dir_url( __FILE__ ) . 'css/wpspeedtestpro-dashboard.css', array(), $this->version, 'all' );
-            wp_enqueue_style( $this->plugin_name . '-dashboard', plugin_dir_url( __FILE__ ) . 'css/wpspeedtestpro-bug-report.css', array(), $this->version, 'all' );
+            wp_enqueue_style( $this->plugin_name . '-bug-report', plugin_dir_url( __FILE__ ) . 'css/wpspeedtestpro-bug-report.css', array(), $this->version, 'all' );
 
         }
     }
@@ -101,7 +101,7 @@ class Wpspeedtestpro_Dashboard {
     public function enqueue_scripts() {
         if ($this->is_this_the_right_plugin_page()) {
             wp_enqueue_script( $this->plugin_name . '-dashboard', plugin_dir_url( __FILE__ ) . 'js/wpspeedtestpro-dashboard.js', array( 'jquery' ), $this->version, false );
-            wp_enqueue_script( $this->plugin_name . '-dashboard', plugin_dir_url( __FILE__ ) . 'js/wpspeedtestpro-bug-report.js', array( 'jquery' ), $this->version, false );
+            wp_enqueue_script( $this->plugin_name . '-bug-report', plugin_dir_url( __FILE__ ) . 'js/wpspeedtestpro-bug-report.js', array( 'jquery' ), $this->version, false );
 
             wp_localize_script($this->plugin_name . '-dashboard', 'wpspeedtestpro_ajax', array(
                 'ajax_url' => admin_url('admin-ajax.php'),
